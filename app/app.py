f7bb4e60b820bb739b9f6f8f6f1cae44cd412cb9
Glenn LEwis <glelewis90@gmail.com>
2024-03-04 21:01:07 -0600

    Added wtforms library and recreated new-task.html

--- app/app.py
+++ app/app.py
@@ -1,17 +1,28 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, DateField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '2YR44XT649dwNmhE'
 
 bootstrap = Bootstrap(app)
+
+class NewTask(FlaskForm):
+      task_name = StringField('Task Name', validators=[DataRequired()])
+      priority = SelectField('Priority', choices=[(1, 'Low'), (2, 'Medium'), (3, 'High'), (4, 'Urgent')], validators=[DataRequired()])
+      due_date = DateField('Due Date', format='%Y-%m-%d', validators=[DataRequired()])
+      submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
       return render_template('index.html')
 
-@app.route('/new-task')
+@app.route('/new-task', methods=['GET', 'POST'])
 def task():
-      return render_template('new-task.html')
+      form = NewTask()
+      return render_template('new-task.html', form=form)
 
 @app.errorhandler(404)
 def page_not_found(e):
